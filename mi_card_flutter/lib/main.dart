0abe690e778a39bcf18ac9bbef194f8bb6167b82
RoTour <rothewawa@gmail.com>
2021-05-03 21:13:47 +0200

    Module 6 Lesson 5 : Challenge Completed

---
+++ mi_card_flutter/lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                color: Colors.red,
+                width: 100,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.greenAccent,
+                  ),
+                ],
+              ),
+              Container(
+                color: Colors.blue,
+                width: 100,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
