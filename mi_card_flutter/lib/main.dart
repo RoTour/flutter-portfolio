8b5ac92bb7b82ba2b59f00f13dff0d875bf49501
RoTour <rothewawa@gmail.com>
2021-05-05 20:29:27 +0200

    Module 6 completed

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -19,6 +19,123 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Row(
+            children: [
+              Expanded(
+                child: Container(),
+                flex: 3,
+              ),
+              Expanded(
+                flex: 4,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: double.infinity,
+                    ),
+                    CircleAvatar(
+                      radius: 50,
+                      backgroundColor: Colors.red,
+                      backgroundImage: AssetImage('images/photopro.jpg'),
+                    ),
+                    Text(
+                      'Robin Tourné',
+                      style: TextStyle(
+                          fontSize: 40,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'KaushanScript'),
+                    ),
+                    Text(
+                      'ETUDIANT - DEVELOPPEUR WEB - FREELANCE',
+                      style: TextStyle(
+                        fontSize: 20,
+                        letterSpacing: 3,
+                        color: Colors.teal.shade100,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'SourceSansPro',
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 100),
+                          child: SizedBox(
+                            height: 20,
+                            child: Divider(
+                              color: Colors.teal.shade100,
+                            ),
+                          ),
+                        ),
+                        Card(
+                          margin: EdgeInsets.symmetric(
+                              horizontal: 30, vertical: 10),
+                          child: ListTile(
+                            leading: Icon(Icons.phone, color: Colors.teal),
+                            title: Text(
+                              '+33 6 29 27 72 29',
+                              style: TextStyle(
+                                fontSize: 20,
+                                letterSpacing: 3,
+                                color: Colors.teal,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'SourceSansPro',
+                              ),
+                            ),
+                          ),
+                        ),
+                        Card(
+                          margin: EdgeInsets.symmetric(
+                              horizontal: 30, vertical: 10),
+                          child: ListTile(
+                            leading: Icon(
+                              Icons.mail,
+                              color: Colors.teal,
+                            ),
+                            title: Text(
+                              'robin.tourne@gmail.com',
+                              style: TextStyle(
+                                fontSize: 20,
+                                letterSpacing: 3,
+                                color: Colors.teal,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'SourceSansPro',
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Container(),
+                flex: 3,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class LayoutChallenge extends StatelessWidget {
+  const LayoutChallenge({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Row(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
